41a29c40e3a5708613f054b35289502f8b34bd82
Adriaan63 <Adcast07@ucm.es>
2023-09-21 12:01:21 +0200

    HE seguido avanzando

--- ProblemaTPV1.cpp
+++ ProblemaTPV1.cpp
@@ -3,6 +3,8 @@
 #include <windows.h>
 #include "Date.h"
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 struct Coche {
@@ -14,12 +16,14 @@ struct Alquiler {
 	int codigo;
 	Date fecha;
 	int dias;
+	Coche* Coche;
 };
 struct ListaCoches
 {
+	int tam = cont + 10;
 	Coche* Coche;
 	int cont;
-	int tam = cont + 10;
+	
 };
 struct ListaAlquileres
 {
@@ -27,12 +31,9 @@ struct ListaAlquileres
 	int cont;
 	int tam = cont + 10;;
 };
-int main() {
-	SetConsoleOutputCP(CP_UTF8);
-}
-bool leerModelos(ListaCoches listaCoches)
+bool leerModelos(ListaCoches& listaCoches)
 {
-	ifstream entrada("coches.txt");
+	ifstream entrada;
 	entrada.open("coches.txt");
 	if (!entrada.is_open()) {
 		return false;
@@ -41,13 +42,69 @@ bool leerModelos(ListaCoches listaCoches)
 	{
 		entrada >> listaCoches.tam;
 		listaCoches.Coche = new Coche[listaCoches.tam];
-		for (int i = 0; !entrada.eof(); i++)
+		for (int i = 0; !entrada.eof() && i < listaCoches.tam; i++)
 		{
 			entrada >> listaCoches.Coche[i].codigo;
 			entrada >> listaCoches.Coche[i].precio;
 			getline(entrada, listaCoches.Coche[i].nombre);
+			//cout << listaCoches.Coche[i].codigo << listaCoches.Coche[i].precio << listaCoches.Coche[i].nombre << endl;
 		}
+		delete[]listaCoches.Coche;
 		return true;
 	}
 }
+int buscarCoche(ListaCoches listaCoches, int codigo) {
+	return 10;
+}
 
+bool leerAlquileres(ListaAlquileres& listaAlquileres,const ListaCoches& listaCoches) 
+{
+	ifstream entrada;
+	entrada.open("rent.txt");
+	if (!entrada.is_open()) {
+		return false;
+	}
+	else {
+		entrada >> listaAlquileres.tam;
+		listaAlquileres.Alquiler = new Alquiler[listaAlquileres.tam];
+		for (int i = 0;!entrada.eof() && i < listaAlquileres.tam; i++) {
+			entrada >> listaAlquileres.Alquiler[i].codigo;
+			entrada >> listaAlquileres.Alquiler[i].fecha;
+			entrada >> listaAlquileres.Alquiler[i].dias;
+			//cout << listaAlquileres.Alquiler[i].codigo << listaAlquileres.Alquiler[i].fecha << listaAlquileres.Alquiler[i].dias << endl;
+			int indice = buscarCoche(listaCoches, listaAlquileres.Alquiler[i].codigo);
+			if ( indice == -1) {
+				listaAlquileres.Alquiler[i].Coche = nullptr;
+			}
+			else {
+				listaAlquileres.Alquiler[i].Coche = &listaCoches.Coche[indice];
+			}
+		}
+		
+		//delete[]listaAlquileres.Alquiler;
+		return true;
+	}
+}
+//bool operator<(const Alquiler& izdo, const Alquiler& dcho) {
+//	// Definición del orden
+//}
+void ordenarAlquileres(const ListaAlquileres& listaAlquileres) {
+	vector<Date> fechas(listaAlquileres.tam);
+	for (int i = 0; i < fechas.size(); i++)
+	{
+		fechas[i] = listaAlquileres.Alquiler[i].fecha;
+	}
+	sort(fechas.begin(), fechas.end());
+	for (auto& elem: fechas) {
+		cout << elem << " ";
+	}
+	
+}
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	ListaCoches Coche;
+	ListaAlquileres Alquileres;
+	leerModelos(Coche);
+	leerAlquileres(Alquileres, Coche);
+	ordenarAlquileres(Alquileres);
+}
